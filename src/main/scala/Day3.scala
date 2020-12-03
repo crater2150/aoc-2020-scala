6ac810e685af49620ac75558a642ef154766969a
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-12-03 13:27:29 +0100

    [day3] solution

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,31 @@
+def day3(trees: List[Vector[Boolean]]): String =
+  treesOnSlope(trees, 3).toString + "\n" + List(
+    treesOnSlopeRec(trees, 1, 1),
+    treesOnSlopeRec(trees, 3, 1),
+    treesOnSlopeRec(trees, 5, 1),
+    treesOnSlopeRec(trees, 7, 1),
+    treesOnSlopeRec(trees, 1, 2),
+    ).product.toString
+
+
+/* Solution with both right and down step as parameter */
+def treesOnSlopeRec(treeMap: List[Vector[Boolean]], rightSlope: Int, downSlope: Int): Long =
+  @annotation.tailrec
+  def go (trees: List[Vector[Boolean]], r: Int, hits: Long): Long =
+    val rest = trees.drop(downSlope) 
+    rest match {
+      case line :: _ => go(rest, r + rightSlope, if line(r % line.size) then hits + 1 else hits)
+      case Nil => hits
+    }
+  go(treeMap, rightSlope, 0)
+
+/* Solution for the first part.
+ * Can also be used for most of part 2, except the one with a down step != 1, 
+ * a different approach was simpler than changing this to accomodate for it
+ */
+def treesOnSlope(trees: List[Vector[Boolean]], rightSlope: Int): Int =
+  val steps = rightSlope to rightSlope * trees.size by rightSlope
+  (
+    for (i, line) <- steps.zip(trees.drop(1))
+    yield line(i % line.size)
+  ).count(identity)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,5 +3,6 @@
   val out = day match {
     case 1 => day1(s.map(_.toInt).toList)
     case 2 => day2(s.toList)
+    case 3 => day3(s.map(_.map(_ == '#').toVector).toList)
   }
   println(out)
