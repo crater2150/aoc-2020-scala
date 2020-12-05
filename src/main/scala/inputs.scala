a9375c1b8d2adb357a2f42b5e71b758b9b3fa493
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-12-05 13:39:50 +0100

    Use inputs lib from 2015 solution

--- src/main/scala/inputs.scala
+++ src/main/scala/inputs.scala
@@ -5,7 +5,19 @@ case class Location(inputDir: String, day: Int)
 
 def inputLines(using l: Location) = scala.io.Source.fromFile(s"${l.inputDir}/day${l.day}.txt").getLines
 
+def simpleInput(solver: String => String)(using l: Location): String =
+  solver(inputLines.mkString)
+
 def input[A](format: String => A = identity)(solver: List[A] => String)(using l: Location): String =
-  solver(inputLines.map(format).toList)
+  inputF(format)(List)(solver)
+
+def inputF[A, C[_]](format: String => A = identity)(coll: collection.Factory[A, C[A]])(solver: C[A] => String)(using l: Location): String =
+  solver(inputLines.map(format).to(coll))
+
+def flatInput[A](format: String => List[A] = List.apply)(solver: List[A] => String)(using l: Location): String =
+  flatInputF(format)(List)(solver)
+
+def flatInputF[A, C[_]](format: String => IterableOnce[A] = identity)(coll: collection.Factory[A, C[A]])(solver: C[A] => String)(using l: Location): String =
+  solver(inputLines.flatMap(format).to(coll))
 
 def boolChar(trueChar: Char): String => Vector[Boolean] = _.map(_ == trueChar).toVector
